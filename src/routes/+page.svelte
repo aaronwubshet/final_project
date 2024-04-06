b086d553796cd8021aca1c7128817d79426014cb
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-04-06 16:25:47 -0400

    set up github pages hosting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Who Owns Boston?</title>
+</svelte:head>
+
+<h1>Who Owns Boston?</h1>
+<p>Below is a map of Boston with the properties owned by the top 10 owners highlighted in different colors</p>
