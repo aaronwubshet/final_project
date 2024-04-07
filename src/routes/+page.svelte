ebdd60c15dc21cf74770abfd21c3912d44f05a76
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-04-07 18:13:50 -0400

    updated top 10 csv dataset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import * as d3 from 'd3';
-	
 	import mapboxgl from "mapbox-gl";
 	import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
 	mapboxgl.accessToken = "pk.eyJ1IjoiYXd1YnNoZXQiLCJhIjoiY2x1b2tiamFpMjAzMjJqbzN6NGQ3bjdwcyJ9.ipL3t_Mw0hLMfZJCF4oW3w";
@@ -18,9 +17,7 @@
 			center: [-71.0589,42.3601]
 		});
 		await new Promise(resolve => map.on("load", resolve));
-		stations = await d3.csv("https://drive.google.com/file/d/1ohWuoCZ_fC5lokRx8jiBXI575DImU4FO/view")
-		// "https://drive.google.com/file/d/1ohWuoCZ_fC5lokRx8jiBXI575DImU4FO/view?usp=sharing");
-			// "https://vis-society.github.io/labs/8/data/bluebikes-stations.csv");
+		stations = await d3.csv("https://raw.githubusercontent.com/aaronwubshet/final_project/main/src/lib/dataset.csv?token=GHSAT0AAAAAACQULPLSDSP7QRAP46U6NWW2ZQTDYNQ");
 	})
 	
 	function getCoords (station) {
@@ -52,7 +49,7 @@
 </svelte:head>
 
 <h1>Who Owns Boston?</h1>
-<p>Below is a map of Boston with the properties owned by the top 10 owners highlighted in different colors {stations.length}</p>
+<!-- <p>Below is a map of Boston with the properties owned by the top 10 owners highlighted in different colors</p> -->
 
 
 <div id="map">	
