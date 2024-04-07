84d66a8bcd1a89870b9e2a482cd62ba851579bbe
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-04-07 19:46:35 -0400

    color coding dots by owners

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,28 +5,34 @@
 	mapboxgl.accessToken = "pk.eyJ1IjoiYXd1YnNoZXQiLCJhIjoiY2x1b2tiamFpMjAzMjJqbzN6NGQ3bjdwcyJ9.ipL3t_Mw0hLMfZJCF4oW3w";
 	import { onMount } from "svelte";
 	
+	let mapViewChanged = 0;
 
 	let map;
-	let stations = [];
+	let rentals = [];
 
 	onMount( async() => {
 		map  = new mapboxgl.Map({
 			container: "map",
 			style: "mapbox://styles/awubshet/cluolh0dx016601pbfkif17ra",
-			zoom: 12,
-			center: [-71.0589,42.3601]
+			zoom: 11.5,
+			center: [-71.0589,42.3601],
+			dragPan: false
 		});
 		await new Promise(resolve => map.on("load", resolve));
-		stations = await d3.csv("https://raw.githubusercontent.com/aaronwubshet/final_project/main/src/lib/top10.csv?token=GHSAT0AAAAAACQULPLTNVOLSFGOBQNXWOVOZQTFD5A");
+		rentals = await d3.csv("https://raw.githubusercontent.com/aaronwubshet/final_project/main/src/lib/top10.csv?token=GHSAT0AAAAAACQULPLT2XVQQ2BZ3EXJWIUQZQTHSWA");
+		// https://raw.githubusercontent.com/aaronwubshet/final_project/main/src/lib/dataset.csv?token=GHSAT0AAAAAACQULPLTVEEFEROYS4OZHXE2ZQTFKNQ") 
 	})
 	
-	function getCoords (station) {
-		let point = new mapboxgl.LngLat(+station.Long, +station.Lat);
+	function getCoords (rental) {
+		let point = new mapboxgl.LngLat(+rental.Long, +rental.Lat);
 		let {x, y} = map.project(point);
 		return {cx: x, cy: y};
 	}
-
-
+	
+	let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+	$: map?.on("move", evt => mapViewChanged++);
+	
+	
 </script>
 
 <style>
@@ -41,7 +47,6 @@
 		height: 100%;
 		pointer-events: none;
 	}
-
 </style>
 
 <svelte:head>
@@ -49,17 +54,23 @@
 </svelte:head>
 
 <h1>Who Owns Boston?</h1>
-<!-- <p>Below is a map of Boston with the properties owned by the top 10 owners highlighted in different colors</p> -->
+<p>Below is a map of Boston with the properties owned by the top 10 owners highlighted in different colors {rentals.OWNER}</p>
+<svg>
 
-
+</svg>
 <div id="map">	
 	<svg>
-		{#each stations as station }
-			<circle 
-				cx={ getCoords(station).cx }
-				cy={ getCoords(station).cy }
-				r="5" 
-				fill="steelblue" />
-		{/each}
+		{#key mapViewChanged}
+			{#each rentals as rental }
+				<circle 
+					cx={ getCoords(rental).cx }
+					cy={ getCoords(rental).cy }
+					r="5" 
+					fill={colorScale(rental.OWNER)}
+				/> 
+		
+			{/each}
+		{/key}
+		
 	</svg>
 </div>
