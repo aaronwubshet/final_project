2454e101345e5030bbbd40a8a3bb757c4e8699d9
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-04-30 18:15:49 -0400

    testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -137,6 +137,7 @@
 		values = addressArray.filter(address => address.includes(query.toLowerCase()));
 		searchedRentals = filteredRentals.filter(rental => values.includes(rental.ADDRESS.toLowerCase()));
 	}
+	$:console.log(searchedRentals);
 	function handleMouseExit2() {
 		hoveredIndex = -1;
 		hoveredOwner = null;
